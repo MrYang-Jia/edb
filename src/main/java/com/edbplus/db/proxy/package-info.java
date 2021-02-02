0a465664fdad124f8ed5e68c670b08f3f6dcef48
yzj <yang123456>
2021-02-02 14:47:39 +0800

    1、添加申明

--- src/main/java/com/edbplus/db/proxy/package-info.java
+++ src/main/java/com/edbplus/db/proxy/package-info.java
@@ -1,4 +1,19 @@
 /**
+ * Copyright (c) 2021 , YangZhiJia 杨志佳 (edbplus@126.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * <p>
  * EDb cglib 代理对象 ；未来可能会通过动态生成源码技术替换，可以一定程度上提高性能，但是基于db的数据返回结果来说，暂时不考虑
  * </p>
