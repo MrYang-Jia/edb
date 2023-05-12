ff517adf92debc6d399d65a2a03d57647fe9f60c
yzj <yzj>
2023-05-12 18:05:26 +0800

    1、调整pg bit(1) 与 boolean 类型填充兼容的实现

--- src/main/java/com/edbplus/db/dialect/EDbPostgreSqlDialect.java
+++ src/main/java/com/edbplus/db/dialect/EDbPostgreSqlDialect.java
@@ -17,6 +17,11 @@ package com.edbplus.db.dialect;
 
 import com.jfinal.plugin.activerecord.dialect.PostgreSqlDialect;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * @ClassName EDbPostgreSqlDialect
  * @Description: postgresql 方言解析器
@@ -54,5 +59,42 @@ public class EDbPostgreSqlDialect extends PostgreSqlDialect {
         }catch (Throwable e){// 请注意这个是获取父级的方法解析，所以无法单独写到工具类里
             return super.replaceOrderBy(sql); // 报错时使用原来的解析方式，以此兼容
         }
+    }
+
+    /**
+     * 数据填充处理
+     * @param pst
+     * @param paras
+     * @throws SQLException
+     */
+    protected void fillStatementHandleDateType(PreparedStatement pst, Object... paras) throws SQLException {
+        for(int i = 0; i < paras.length; ++i) {
+            Object value = paras[i];
+            if (value instanceof Date) {
+                if (value instanceof java.sql.Date) {
+                    pst.setDate(i + 1, (java.sql.Date)value);
+                } else if (value instanceof Timestamp) {
+                    pst.setTimestamp(i + 1, (Timestamp)value);
+                } else {
+                    Date d = (Date)value;
+                    pst.setTimestamp(i + 1, new Timestamp(d.getTime()));
+                }
+            }else  if (value instanceof Boolean) { // postgres bit(1) 与 boolean 类型一致的转换方式
+                pst.setObject(i + 1, boolToStringInt((Boolean) value));
+            }
+            else {
+                pst.setObject(i + 1, value);
+            }
+        }
+
+    }
+
+    /**
+     * boolean 转字符串
+     * @param b
+     * @return
+     */
+    public String boolToStringInt(Boolean b) {
+        return b ? "1" : "0";
     }
 }
